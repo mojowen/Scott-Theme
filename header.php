0c5b18cb6f8085b6f5c63e542b39cb1f1a3d3389
Scott Duncombe <srduncombe@gmail.com>
2013-08-21 22:57:01 -0700

    fixing merge mixup

--- header.php
+++ header.php
@@ -10,19 +10,19 @@
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
-	<html class="ie ie7" <?php language_attributes(); ?>>
+    <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-	<html class="ie ie8" <?php language_attributes(); ?>>
+    <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
-	<html <?php language_attributes(); ?>>
+    <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 
 <head>
- 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-  	<title><?php echo srd_title(); ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php echo srd_title(); ?></title>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/stuff/icon.ico" />
   <?php wp_head(); ?>
   <?php open_graph_crap(); ?>
@@ -33,31 +33,24 @@
   <div id="page" >
 
     <header id="menu">
-    	<h1><a href="/">Scott Riker Duncombe</a> :: <span id="title"><?php echo srd_title(false); ?></span></h1> <!-- should change depending on the subpage -->
+        <h1><a href="/">Scott Riker Duncombe</a> :: <span id="title"><?php echo srd_title(false); ?></span></h1> <!-- should change depending on the subpage -->
     </header>
     <sidebar id="side">
-        <a id="photo" href="/about"><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/me.jpeg"></a>
-    	<h4 id="description"><?php echo get_bloginfo('description'); ?></h4>
-    	<a id="about" href="/about">What does that mean?</a>
+        <a id="photo" href="/about"><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/me.jpg"></a>
+        <h4 id="description"><?php echo get_bloginfo('description'); ?></h4>
         <a id="projects_link" href="/">Here's some cool stuff I've built</a>
-    	<div id="social">
-    		<a href="https://github.com/mojowen" target='_blank'>github</a>
-            <a href="http://sduncombe.tumblr.com/" target='_blank'>blog</a>
-    		<a href="/resume" target='_blank'>resume</a>
-    		<a href="https://facebook.com/srduncombe" target='_blank'>fb</a>
-    		<a href="https://twitter.com/sduncombe" target='_blank'>tw</a>
-    		<a href="https://www.linkedin.com/in/scottduncombe/" target='_blank'>ln</a>
-    	</div>
-    	<div id="ask">
-			<h5>Want to Build Something</h5>
-			<a href="/contact"><h4>Awesome</h4></a>
-			<h5>Together?</h5>
-    		</h5>
-    	</div>
-    	<div id="peeps">
-    		<h5>Some of the cats I work with<br/>to make amazing things:</h5>
-            <a href="http://mollieruskin.com/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/mollie.png"><br/>Mollie Ruskin,<br/>Washington DC</div></a>
-            <a href="http://noahmanger.com/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/noah.jpeg"><br/>Noah Manger,<br/>Portland OR</div></a>
-            <a href="http://cargocollective.com/bryanchiem/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/bryan.JPG"><br/>Bryan Chiêm,<br/>California</div></a>
-    	</div>
+        <div id="social">
+            <a href="https://github.com/" target='_blank'>github</a>
+            <a href="http://tumblr.com/" target='_blank'>blog</a>
+            <!-- <a href="/" target='_blank'>resume</a> -->
+            <a href="https://facebook.com/" target='_blank'>fb</a>
+            <a href="https://twitter.com/" target='_blank'>tw</a>
+            <a href="https://www.linkedin.com/in//" target='_blank'>ln</a>
+        </div>
+        <div id="peeps">
+            <h5>Some of the cats I work with<br/>to make amazing things:</h5>
+            <a href="/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/friend_1.jpg"><br/>One Friend,<br/>Friend's Place</div></a>
+            <a href="/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/friend_2.jpg"><br/>Two Friend,<br/>Friend's Place</div></a>
+            <a href="/" target="_blank"><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/stuff/friend_3.jpg"><br/>Three Friend,<br/>Friend's Place</div></a>
+        </div>
     </sidebar>
