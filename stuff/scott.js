c99455c2d6d500a15fcfb60acc32252fbca00520
Scott Duncombe <srduncombe@gmail.com>
2017-09-30 19:50:52 +0000

    Updates from the server

--- stuff/scott.js
+++ stuff/scott.js
@@ -8,6 +8,10 @@ window.onload = function() {
 			ignore_popstate = false;
 
 		function close() {
+            var nextSib = openThumbnail.parentElement.nextElementSibling
+            nextSib.style.zIndex = -1
+            setTimeout(function() { nextSib.style.zIndex = null; }, 1010)
+
 			openThumbnail.style.margin = '0';
 			openThumbnail.style.width = null;
 			openThumbnail.className = 'project';
@@ -161,8 +165,9 @@ window.onload = function() {
 				img.onclick = function() { try{ ga('send', 'event', 'open '+img.src, 'clicked'); } catch(e) { }; window.open(img.src); }
 				galleryImage.className += ' active';
 
-				heading.text = galleryImage.alt;
-				heading.textContent = galleryImage.alt;
+                var galleryCaption = galleryItem.querySelector('.gallery-caption')
+				heading.textContent = galleryCaption.textContent;
+
 				if( considerMobile() && isEvent ) { try{ ga('send', 'event', 'open '+img.src, 'clicked'); } catch(e) { }; window.open(img.src);  }
 				else { try{ ga('send', 'event', 'preview '+img.src, 'clicked'); } catch(e) { }; }
 			}
